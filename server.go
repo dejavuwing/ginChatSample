c54ead14eca0f2fb19b4fb5e302bb6cd177ad5bd
Tongchun <tongchun@ngle.co.kr>
2019-02-16 17:24:52 +0900

    chat server

---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	r.SetHTMLTemplate(template.Must(template.ParseFiles("index.html")))
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	h := newHub()
+	wsh := wsHandler{h: h}
+	r.GET("/ws", func(c *gin.Context) {
+		wsh.ServeHTTP(c.Writer, c.Request)
+	})
+	log.Printf("serving on port 8070")
+	r.Run(":8070")
+}
